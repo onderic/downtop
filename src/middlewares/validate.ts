ae7cfda826a7e6d413378abd7c88271d179bd20e
Ian Mukua <ianmukuaa@gmail.com>
2024-11-13 13:54:33 +0300

    Refactor: Update validation middleware to use Zod schema

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,20 +1,20 @@
 import httpStatus from 'http-status';
 import ApiError from '../utils/ApiError';
 import { NextFunction, Request, Response } from 'express';
+import { ZodSchema } from 'zod';
 import pick from '../utils/pick';
-import Joi from 'joi';
 
-const validate = (schema: object) => (req: Request, res: Response, next: NextFunction) => {
-  const validSchema = pick(schema, ['params', 'query', 'body']);
-  const obj = pick(req, Object.keys(validSchema));
-  const { value, error } = Joi.compile(validSchema)
-    .prefs({ errors: { label: 'key' }, abortEarly: false })
-    .validate(obj);
-  if (error) {
-    const errorMessage = error.details.map((details) => details.message).join(', ');
+const validate = (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+  const validatedData = pick(req, ['body', 'params', 'query']);
+  const validationResult = schema.safeParse(validatedData);
+
+  if (!validationResult.success) {
+    const errorMessage = validationResult.error.errors.map((error) => error.message).join(', ');
     return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
   }
-  Object.assign(req, value);
+
+  // Assign validated values back to req
+  Object.assign(req, validationResult.data);
   return next();
 };
 
