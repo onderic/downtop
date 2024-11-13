0ea04fe6058827ff03af9c3c86a70d0d0d5f0bb2
Ian Mukua <ianmukuaa@gmail.com>
2024-11-13 21:23:47 +0300

    Add: user controller export

---
+++ src/controllers/users/user.conroller.ts
@@ -0,0 +1,60 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import { userService } from '../../services';
+import { Role } from '@prisma/client';
+import ApiError from '../../utils/ApiError';
+
+const createUser = catchAsync(async (req, res) => {
+  const { contact, password, username, role } = req.body;
+  const user = await userService.createUser({ contact, password, username, role });
+  res.status(httpStatus.CREATED).send(user);
+});
+
+const updateUser = catchAsync(async (req, res) => {
+  const { userId } = req.params;
+  if (Object.keys(req.body).length === 0) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'User data is required');
+  }
+  const user = await userService.updateUser(userId, req.body);
+  res.send(user);
+});
+
+const getAllUsers = catchAsync(async (req, res) => {
+  interface QueryParams {
+    limit?: string;
+    page?: string;
+    role?: Role;
+    endDate?: string;
+    startDate?: string;
+  }
+  const { limit = '10', page = '1', role, endDate, startDate } = req.query as QueryParams;
+  const typedRole = role as Role | undefined;
+  const result = await userService.getAllUsers({
+    limit: parseInt(limit, 10),
+    page: parseInt(page, 10),
+    role: typedRole,
+    startDate,
+    endDate
+  });
+  res.send(result);
+});
+
+const getUser = catchAsync(async (req, res) => {
+  const { userId } = req.params;
+  const user = await userService.getUser(userId);
+  res.send(user);
+});
+
+const deleteUser = catchAsync(async (req, res) => {
+  const { userId } = req.params;
+  await userService.deleteUser(userId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+export default {
+  createUser,
+  updateUser,
+  getAllUsers,
+  getUser,
+  deleteUser
+};
