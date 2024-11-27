208f4315df48b23c214c41885b3263a640dd1999
onderic <ondericollins80@gmail.com>
2024-11-27 12:03:29 +0300

    otp callback done

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -2,12 +2,28 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import { Request, Response } from 'express';
 import authService from '../../services/auth.service';
+import tokenService from '../../services/token.service';
 import { Login } from '../../types/auth.types';
+import userService from '../../services/user.service';
+import exclude from '../../utils/exclude';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const loginData: Login = req.body;
-  const { user, tokens } = await authService.loginUser(loginData);
-  res.status(httpStatus.OK).json({ user, tokens });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const user = await authService.loginUser(loginData);
+  res.status(httpStatus.OK).json({ message: 'user available' });
+});
+
+const verifyOTPtoken = catchAsync(async (req: Request, res: Response) => {
+  const { phone, otp } = req.body;
+  const user = await userService.getUser({ phone: phone });
+  if (!user) {
+    return;
+  }
+  await tokenService.verifyOTP(user?.id, otp);
+  const tokens = await tokenService.genAuthtokens(user.id);
+  const userWithoutPassword = exclude(user, ['password']);
+  res.status(httpStatus.OK).json({ user: userWithoutPassword, tokens });
 });
 
 const logoutUser = catchAsync(async (req: Request, res: Response) => {
@@ -25,5 +41,6 @@ export const refreshToken = catchAsync(async (req: Request, res: Response) => {
 export default {
   loginUser,
   logoutUser,
-  refreshToken
+  refreshToken,
+  verifyOTPtoken
 };
