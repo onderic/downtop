a7723b79ba27b0b297bb18ba8166ba4b2fa2c646
Ian Mukua <ianmukuaa@gmail.com>
2024-12-18 20:23:51 +0300

    Refactor payment and subscription routes to include validation for MpesaStkRequest and subscription schemas

--- src/routes/v1/payment/subscription.route.ts
+++ src/routes/v1/payment/subscription.route.ts
@@ -1,24 +1,30 @@
 import { Router } from 'express';
 import auth from '../../../middlewares/auth';
 import { subscriptionController } from '../../../controllers';
+import validate from '../../../middlewares/validate';
+import { newPlanSchema, updatePlanSchema } from '../../../types/subscription';
 
 const router = Router();
 
 router.route('/').get(auth('getAllSubscriptions'), subscriptionController.getAllSubscriptions);
+
 router
-  .route('/:shopId')
+  .route('/shop/:shopId')
   .get(auth('getShopSubscriptions'), subscriptionController.getShopSubscriptions);
+
 router
-  .route('/:subscriptionId')
+  .route('/id/:subscriptionId')
   .get(auth('getSubscription'), subscriptionController.getSubscriptionById);
+
 router
   .route('/plans')
   .get(auth('getAllPlans'), subscriptionController.getAllPlans)
-  .post(auth('createPlan'), subscriptionController.createPlan);
+  .post(auth('createPlan'), validate(newPlanSchema), subscriptionController.createPlan);
+
 router
   .route('/plans/:planId')
   .get(auth('getPlan'), subscriptionController.getPlanById)
-  .patch(auth('updatePlan'), subscriptionController.updatePlan)
+  .patch(auth('updatePlan'), validate(updatePlanSchema), subscriptionController.updatePlan)
   .delete(auth('deletePlan'), subscriptionController.deletePlan);
 
 export default router;
